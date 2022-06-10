449223d01ef9937a5ab182647cb2b9ff2af33ee0
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-06-10 16:21:18 -0500

    CV y foto agregada

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import pdf from "../docs/CV.pdf";
+import img from "../asset/img/Juan Valencia byn.jpg";
 
 export const Home = () => {
   const [t] = useTranslation("global");
@@ -22,10 +23,7 @@ export const Home = () => {
         </div>
       </div>
       <div className="home__img">
-        <img
-          src="https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-          alt="img"
-        />
+        <img src={img} alt="img" />
       </div>
     </div>
   );
