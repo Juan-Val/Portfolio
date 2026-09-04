15a07fea6923d12e8c7f71f37798f609dbe6417a
Juan Valencia Olvera <juan.vo.11.2000@gmail.com>
2026-09-04 01:02:07 -0600

    Reset cambios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import pdf from "../docs/CV-2026.pdf";
+import pdf from "../docs/CV.pdf";
 
 export const Home = () => {
   const [t] = useTranslation("global");
